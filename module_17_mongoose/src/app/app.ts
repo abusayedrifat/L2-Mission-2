3a568dadd27b6fe49ec685a4176f3c66eb643e1e
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-02-14 23:40:07 +0600

    update

--- module_17_mongoose/src/app/app.ts
+++ module_17_mongoose/src/app/app.ts
@@ -9,20 +9,39 @@ app.get('/',(req:Request, res:Response)=>{
     res.send('suever running through mongoose')
 })
 
+// const noteSchema = new Schema({
+//     title: String, //todo: this is shorthand types. we should avoid this and conventional object type types
+//     content: {type:String, default:''} //todo: conventional types
+// })
 const noteSchema = new Schema({
-    title: String,
-    content: String
+    title:{type: String, require:true},
+    content:{type: String, default:''},
+    category:{
+        type: String,
+        enum: ['personal', 'work', 'study', 'research'],
+        default:'personal'
+    },
+    pinned:{ type: Boolean, default:false },
+    tags:{
+        label:{type: String, require:true},
+        color:{type: String, default:'gray'}
+    }
 })
 
 
 const Note = model('Note', noteSchema) // Note variable works like as a 'class'
 
-app.post('/create-post',(req:Request, res:Response)=>{
+app.post('/create-post',async (req:Request, res:Response)=>{
     
    const myNote = new Note({
         title:'learinig mongoose',
-        content:'it is fun to learn mongoose'
+        content:'it is fun to learn mongoose',
+        tags:{
+            label:'story'
+        }
     })
+
+    await myNote.save()
 
     res.status(201).json({
         success: true,
