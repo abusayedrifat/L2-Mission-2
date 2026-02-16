373b587fe37ea40c856221645f9019b6c1cd967a
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-02-17 00:04:02 +0600

    users CRUD completed

--- module_17_mongoose/src/app/controller/user_routes.ts
+++ module_17_mongoose/src/app/controller/user_routes.ts
@@ -9,9 +9,34 @@ usersRoutes.get('/',async(req:Request,  res:Response)=>{
     res.json(allUsers)
 })
 
+usersRoutes.get('/:userID',async(req:Request,  res:Response)=>{
+    
+    const userID = req.params.userID;
+    const user = await Users.findById(userID);
+    res.json(user)
+})
+
 usersRoutes.post('/create-user',async(req:Request,  res:Response)=>{
     
     const body = req.body
     const allUsers = await Users.create(body)
     res.json(allUsers)
+})
+
+usersRoutes.patch('/update/:updateID',async(req:Request,  res:Response)=>{
+    
+    const updateID = req.params.updateID
+    const updateBody = req.body
+    const updateUser = await Users.findByIdAndUpdate(updateID, updateBody,{new:true})
+    res.json(updateUser)
+})
+
+usersRoutes.delete('/delete/:deleteID',async(req:Request,  res:Response)=>{
+    
+    const deleteID = req.params.deleteID;
+    const deleteUser = await Users.findByIdAndDelete(deleteID)
+    res.json({
+        message:'deleted successfuly',
+        deleteUser
+    })
 })
--- module_17_mongoose/src/app/schema/users_schema.ts
+++ module_17_mongoose/src/app/schema/users_schema.ts
@@ -3,17 +3,23 @@ import { users } from "../interfaces/user_interfaces";
 
 export const usersSchema = new Schema<users>({
     name: {
-        firstName: { type: String, required: true },
-        lastName: { type: String, required: true }
+        firstName: { type: String, required: true, trim:true },
+        lastName: { type: String, required: true , trim:true}
     },
     address: { type: String , required:true},
-    email: { type: String, required: true },
+    email: { type: String, required: true , trim:true},
     role: {
         type: String,
         enum: ["user", "admin"],
         default: "user"
     }
-})
+},
+
+{
+    versionKey:false,
+    timestamps:true
+}
+)
 
 
  export const Users =  model('Users', usersSchema )
